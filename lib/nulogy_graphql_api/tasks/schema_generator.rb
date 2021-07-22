ec646a3ddc2174b57752d7561972a02f5cafbcd6
Allan Kielstra <allank@nulogy.com>
2021-07-22 14:40:40 -0400

    correct-schema: Get and write contents of schema from schema object

--- lib/nulogy_graphql_api/tasks/schema_generator.rb
+++ lib/nulogy_graphql_api/tasks/schema_generator.rb
@@ -1,9 +1,9 @@
 module NulogyGraphqlApi
   module Tasks
     class SchemaGenerator
-      def initialize(schema_output_path, schema_definition, context: {})
+      def initialize(schema_output_path, schema, context: {})
         @schema_output_path = schema_output_path
-        @schema_definition = schema_definition
+        @schema = schema
         @context = context.merge(
           schema_generation_context?: true
         )
@@ -19,7 +19,7 @@ module NulogyGraphqlApi
       def check_changes
         return if old_schema.blank?
 
-        SchemaChangesChecker.new.check_changes(old_schema, @schema_definition)
+        SchemaChangesChecker.new.check_changes(old_schema, @schema)
       end
 
       def old_schema
@@ -29,8 +29,12 @@ module NulogyGraphqlApi
       end
 
       def write_schema_to_file
-        File.write(@schema_output_path, @schema_definition)
+        File.write(@schema_output_path, schema_definition)
         puts Rainbow("\nSuccessfully updated #{@schema_output_path}").green
+      end
+
+      def schema_definition
+        GraphQL::Schema::Printer.print_schema(@schema, context: @context)
       end
     end
   end
