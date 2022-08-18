2d9fec1b0d1a9431f45ce6218898f880a39ccd62
Ashik Varghese <ashikv@nulogy.com>
2022-08-18 10:47:32 -0400

    Try: Int instead of Integer

--- lib/nulogy_graphql_api/types/path_integer_type.rb
+++ lib/nulogy_graphql_api/types/path_integer_type.rb
@@ -1,6 +1,6 @@
 module NulogyGraphqlApi
   module Types
-    class PathIntegerType < ::GraphQL::Schema::Integer
+    class PathIntegerType < GraphQL::Schema::Int
       description "Represents integer elements in the path array as part of UserErrorType"
 
       field :path, Integer, null: false
