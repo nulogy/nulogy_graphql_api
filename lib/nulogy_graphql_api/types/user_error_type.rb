e7809f77e55068ec12ea7be7eaf740c5ee93cb05
Ashik Varghese <ashikv@nulogy.com>
2022-08-18 13:40:13 -0400

    Try: Return PathStringType in path

--- lib/nulogy_graphql_api/types/user_error_type.rb
+++ lib/nulogy_graphql_api/types/user_error_type.rb
@@ -5,7 +5,7 @@ module NulogyGraphqlApi
 
       field :message, String, null: false,
         description: "A description of the error"
-      field :path, [String], null: false,
+      field :path, [NulogyGraphqlApi::Types::PathStringType], null: false,
         description: "Which input value this error came from"
     end
   end
