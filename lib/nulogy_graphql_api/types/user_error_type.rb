f2f097a0a1a95a0ed3a2794cbdfd99dea60c8a3a
Ashik Varghese <ashikv@nulogy.com>
2022-08-18 13:30:39 -0400

    Try: Reverting to returning String in path

--- lib/nulogy_graphql_api/types/user_error_type.rb
+++ lib/nulogy_graphql_api/types/user_error_type.rb
@@ -5,7 +5,7 @@ module NulogyGraphqlApi
 
       field :message, String, null: false,
         description: "A description of the error"
-      field :path, [NulogyGraphqlApi::Types::PathType], null: false,
+      field :path, [String], null: false,
         description: "Which input value this error came from"
     end
   end
