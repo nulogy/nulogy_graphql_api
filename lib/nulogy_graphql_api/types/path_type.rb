59ac93beaf1be47a4c59c17ce45c487cf70a6465
Ashik Varghese <ashikv@nulogy.com>
2022-08-18 14:14:20 -0400

    Try: Scoping in PathType

--- lib/nulogy_graphql_api/types/path_type.rb
+++ lib/nulogy_graphql_api/types/path_type.rb
@@ -1,15 +1,20 @@
 module NulogyGraphqlApi
   module Types
     class PathType < NulogyGraphqlApi::Types::BaseUnion
-      possible_types Integer, String
+      possible_types PathIntegerType, PathStringType
 
-      # def self.resolve_type(object, _context)
-      #   if object.is_a?(PathStringType)
-      #     NulogyGraphqlApi::Types::PathStringType
-      #   else
-      #     NulogyGraphqlApi::Types::PathIntegerType
-      #   end
-      # end
+      def self.resolve_type(object, _context)
+        p '*' * 80
+        p object
+        p object.is_a?(PathStringType)
+        p object.class
+
+        if object.is_a?(NulogyGraphqlApi::Types::PathStringType)
+          NulogyGraphqlApi::Types::PathStringType
+        else
+          NulogyGraphqlApi::Types::PathIntegerType
+        end
+      end
     end
   end
 end
